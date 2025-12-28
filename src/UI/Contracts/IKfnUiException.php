94d7baa9701489e402b5db8852dec8c949b3902a
Much Yusron Arif <yusron.arif4@gmail.com>
2025-12-28 04:44:26 +0000

    update ui exception interface

--- src/UI/Contracts/IKfnUiException.php
+++ src/UI/Contracts/IKfnUiException.php
@@ -2,6 +2,21 @@
 
 namespace Kfn\UI\Contracts;
 
+use Kfn\Base\Contracts\IResponseCode;
+
 interface IKfnUiException
 {
+    public function exist(): bool;
+
+    public function getResponseCode(): IResponseCode|null;
+
+    public function getName(): string|null;
+
+    public function getMessage(): string|null;
+
+    public function getCode(): int|null;
+
+    public function getStatusText(): string|null;
+
+    public function toArray(): array;
 }
